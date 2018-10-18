3c1f9557c30b68ac992b22f6ee62c54cda1d17f8
tu_nv <tu_nv@septeni-techonology.jp>
2018-10-18 18:11:30 +0700

    add AI, condition win and draw

--- src/App.js
+++ src/App.js
@@ -2,6 +2,41 @@
 import { Client } from 'boardgame.io/react';
 import { Game } from 'boardgame.io/core';
 import TicTacToeBoard from './components/MainGame/TicTacToeBoard'
+import { AI } from 'boardgame.io/ai';
+
+// Return true if `cells` is in a winning configuration.
+function IsVictory(cells) {
+    const positions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let pos of positions) {
+        const symbol = cells[pos[0]];
+        let winner = symbol;
+        for (let i of pos) {
+            if (cells[i] !== symbol) {
+                winner = null;
+                break;
+            }
+        }
+        if (winner != null) return true;
+    }
+
+    return false;
+}
+
+// Return true if all `cells` are occupied.
+function IsDraw(cells) {
+    return cells.filter(c => c === null).length === 0;
+}
+
 
 const TicTacToe = Game({
     setup: () => ({ cells: Array(9).fill(null) }),
@@ -17,12 +52,12 @@ const TicTacToe = Game({
     flow: {
         movesPerTurn: 1,
         endGameIf: (G, ctx) => {
-            // if (IsVictory(G.cells)) {
-            //     return { winner: ctx.currentPlayer };
-            // }
-            // if (IsDraw(G.cells)) {
-            //     return { draw: true };
-            // }
+            if (IsVictory(G.cells)) {
+                return { winner: ctx.currentPlayer };
+            }
+            if (IsDraw(G.cells)) {
+                return { draw: true };
+            }
         },
     },
 
@@ -31,19 +66,19 @@ const TicTacToe = Game({
 
 const App = Client({
     game: TicTacToe ,
-    board: TicTacToeBoard
+    board: TicTacToeBoard,
 
-    // ai: AI({
-    //     enumerate: (G, ctx) => {
-    //         let moves = [];
-    //         for (let i = 0; i < 9; i++) {
-    //             if (G.cells[i] === null) {
-    //                 moves.push({ move: 'clickCell', args: [i] });
-    //             }
-    //         }
-    //         return moves;
-    //     },
-    // }),
+    ai: AI({
+        enumerate: (G, ctx) => {
+            let moves = [];
+            for (let i = 0; i < 9; i++) {
+                if (G.cells[i] === null) {
+                    moves.push({ move: 'clickCell', args: [i] });
+                }
+            }
+            return moves;
+        },
+    }),
 });
 
 export default App;
